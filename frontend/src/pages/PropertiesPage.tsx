eb3a150002a80a7827eca28d53d27a8a8a2f7fb6
ganantia-a11y <g.anantia@digitaloriented.it>
2026-05-06 15:00:21 +0200

    Add Properties page

---
+++ frontend/src/pages/PropertiesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropertyList from '../components/PropertyList';
+
+const PropertiesPage: React.FC = () => {
+  return (
+    <div>
+      <PropertyList />
+    </div>
+  );
+};
+
+export default PropertiesPage;
